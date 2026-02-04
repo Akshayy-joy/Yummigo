d3dbef0ac8dd26fda8fa3d1f227a495a0415bca4
murugesantechver <murugeshan.r@techversantinfotech.com>
2026-02-04 14:41:20 +0530

    chore: remove all source files and configuration for yummigo project

--- src/utils/generateToken.ts
+++
@@ -1,6 +0,0 @@
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config/env";
-
-export const generateToken = (userId: number) => {
-  return jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: "7d" });
-};
